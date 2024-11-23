b6277e4783c5e27ce646f317c4c82211cd55351f
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2024-11-23 15:01:55 +0300

    add

--- schemas/openInterestSchema.js
+++ schemas/openInterestSchema.js
@@ -23,11 +23,12 @@ const customerSchema = new Schema(
       type: Boolean,
       default: true,
     },
-    paymentId: {type: String, default: ''},
-    monthes: {type: Number, default: 0},
+    paymentId: { type: String, default: "" },
+    monthes: { type: Number, default: 0 },
     expireTime: {
       type: Date,
-      default: () => new Date(Date.now() + 72 * 60 * 60 * 1000),
+      default: () => new Date(Date.now() + 5 * 1000),
+      // default: () => new Date(Date.now() + 72 * 60 * 60 * 1000),
     },
   },
   { versionKey: false, timestamps: false }
